e3fbf2876d32d6ef05fc513be392c512a1c385cb
iosninjamaster <iosninjamaster@gmail.com>
2014-06-02 20:48:16 -0600

    initial commit

---
+++ Bendiciones Ordenanzas Sacerdocio SUD/Blessing.h
@@ -0,0 +1,13 @@
+//
+//  Blessing.h
+//  Bendiciones Ordenanzas Sacerdocio SUD
+//
+//  Created by iosninjamaster on 4/15/14.
+//  Copyright (c) 2014 app-candi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Blessing : NSObject
+
+@end
---
+++ Bendiciones Ordenanzas Sacerdocio SUD/Blessing.m
@@ -0,0 +1,13 @@
+//
+//  Blessing.m
+//  Bendiciones Ordenanzas Sacerdocio SUD
+//
+//  Created by iosninjamaster on 4/15/14.
+//  Copyright (c) 2014 app-candi. All rights reserved.
+//
+
+#import "Blessing.h"
+
+@implementation Blessing
+
+@end
