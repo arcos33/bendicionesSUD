f72c6e570d6a3744976ad1dc038e15d8780284e3
iosninjamaster <iosninjamaster@Joels-MacBook-Pro.local>
2014-06-24 23:40:02 -0600

    first commit

---
+++ OrganizacionViewController.h
@@ -0,0 +1,13 @@
+//
+//  OrganizacionViewController.h
+//  Bendiciones Ordenanzas Sacerdocio SUD
+//
+//  Created by iosninjamaster on 6/9/14.
+//  Copyright (c) 2014 app-candi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrganizacionViewController : UIViewController
+
+@end
---
+++ OrganizacionViewController.m
@@ -0,0 +1,49 @@
+//
+//  OrganizacionViewController.m
+//  Bendiciones Ordenanzas Sacerdocio SUD
+//
+//  Created by iosninjamaster on 6/9/14.
+//  Copyright (c) 2014 app-candi. All rights reserved.
+//
+
+#import "OrganizacionViewController.h"
+
+@interface OrganizacionViewController ()
+
+@end
+
+@implementation OrganizacionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
